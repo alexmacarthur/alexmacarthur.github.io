d984ded31b898fb553f861b6e83b6d2692106f8d
Alex MacArthur <alex@macarthur.me>
2015-11-22 20:06:24 -0600

    Lots of reworking, contact form up.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,17 @@ var viewportHeight = $(window).height();
 $(document).ready(function(){
 	var splitHeight = viewportHeight/$('.HomeSection').length;
 
-	$('.HomeSection').css('height',viewportHeight);
+	$('.HomeSection').css('min-height',viewportHeight);
+
+	(function(){
+		var $homeHeader = $('.HomeSection--main .HomeSection-container');
+		var newPadding = ($(window).height() - $homeHeader.innerHeight())/2;
+		$homeHeader.css('padding-top',newPadding);
+	})()
 
 	$('.HomeHeader').typeIt({
 		typeSpeed: 125,
-	  whatToType: ["test"]
+	  	whatToType: ["Hi, I'm Alex MacArthur."]
 	}, function(){
 		$('.SocialNav-item').addClass('animation-popup');
 	});
@@ -19,6 +25,27 @@ $(document).ready(function(){
 	currentMenuLink();
 	$(window).scroll(function() {
 		currentMenuLink();
+	});
+
+	$('#ContactForm').submit(function(e){
+		e.preventDefault();
+		$.ajax({
+		    url: "//formspree.io/alex@macarthur.me", 
+		    method: "POST",
+		    data: {message: "hello!"},
+		    dataType: "json"
+		}).done(function(response) {
+
+            // Clear the form.
+            $('input[name="name"]').val('');
+            $('input[name="_replyto"]').val('');
+            $('textarea[name="message"]').val('');
+
+            $('.ContactForm-submit').prop('disabled', true);
+            $('#StatusMessages').html('Your message was successfully sent! Thanks.').removeClass('failure').addClass('success');
+        }).fail(function(data) {
+        	$('#StatusMessages').html('Sorry, an something\'s messed up. Refresh the page to try again, or just send an email to alex@macarthur.me.').removeClass('success').addClass('failure');
+        });
 	});
 
 });
