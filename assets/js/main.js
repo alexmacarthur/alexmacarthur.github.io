97f1d2c1904ef147657f79686dea6ac4881c2a3c
Alex MacArthur <alex@macarthur.me>
2015-12-14 22:26:15 -0600

    ExpeExperiment with new scroller.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,28 @@ var heightIssue = false;
 
 $(document).ready(function(){
 
+	$(".main").onepage_scroll({
+	   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+	   easing: "ease-in",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",                             // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+	   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+	   pagination: false,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+	   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+	   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+	   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+	   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+	   keyboard: true,                  // You can activate the keyboard controls
+	   responsiveFallback: 600,        // You can fallback to normal page scroll by defining the width of the browser in which
+	                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+	                                    // the browser's width is less than 600, the fallback will kick in.
+	   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+	});
+
 	// check if any of the HomeSections are taller than the viewport
-	checkForHeightIssue();
+	// checkForHeightIssue();
+
 	// initilize snap scrolling
 	if(heightIssue !== true){
-		initScrollify();
+		//initScrollify();
 	}
 	// set top section to screen height
 	$top.css('height', viewportHeight);
@@ -22,10 +39,10 @@ $(document).ready(function(){
 		// set top section to screen height
 		$top.css('height', viewportHeight);
 		// check if any of the HomeSections are taller than the viewport
-		checkForHeightIssue();
+		//checkForHeightIssue();
 		// initilize snap scrolling
 		if(heightIssue !== true){
-			initScrollify();
+			//initScrollify();
 		}
 	});
 
@@ -67,7 +84,12 @@ function checkForHeightIssue() {
 	var $homeSections = $('.HomeSection--notTop');
 	$homeSections.each(function(){
 		var thisHeight = $(this).find('.HomeSection-wrapper').height();
+
+		console.log('Section: ' + thisHeight);
+		console.log('VP: ' + viewportHeight);
+
 		if(thisHeight >= viewportHeight){
+			console.log($(this).find('.HomeSection-wrapper'));
 			heightIssue = true;
 			$('#main').addClass('has-height-issue');
 		}
@@ -83,11 +105,11 @@ function initTypeIt() {
 	});
 }
 
-function initScrollify() {
-	$.scrollify({
-			section : ".HomeSection"
-	});
-}
+// function initScrollify() {
+// 	$.scrollify({
+// 			section : ".HomeSection"
+// 	});
+// }
 
 function initCurrentMenuLink() {
 	currentMenuLink();
@@ -156,7 +178,7 @@ function initContactForm() {
 }
 
 function scrollTo(section){
-	$.scrollify.move("#"+section);
+	//$.scrollify.move("#"+section);
 }
 
 function currentMenuLink(){
